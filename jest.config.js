012676b6da1bfdf6e00e50571a99d499433b90d4
JOEL DE JESUS LOPEZ RUIZ <joelssj93@gmail.com>
2024-10-27 20:24:56 -0600

    first commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  testMatch: ['<rootDir>/src/tests/**/*.test.ts'],  // Utiliza rootDir para que Jest busque en la raíz.
+};
---
+++ readme.md
@@ -0,0 +1,10 @@
+Integrantes del equipo:
+
+221204 - Joel de Jesus Lopez Ruiz
+221199 - Carlos Eduardo Gumeta Navarro
+221198 - Jesus Alejandro Guillen Luna
+
+
+
+npm install puppeteer@latest whatsapp-web.js@latest
+npm install whatsapp-web.js@latest
